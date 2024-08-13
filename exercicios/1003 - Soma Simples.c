cd1d9f866d0c10626b6a89755a891434413c9f28
stephanieribas <stephanieribas2007@gmail.com>
2024-08-12 22:15:44 -0300

    exercicios

---
+++ exercicios/1003 - Soma Simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A = 0;
+    int B = 0;
+    int SOMA = 0;
+    
+    scanf("%d\n%d", &A, &B);
+    
+    SOMA = A+B;
+    
+    printf("SOMA = %d\n", SOMA);
+ 
+    return 0;
+}
---
+++ exercicios/1010 - Cálculo Simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+ int peca = 0, numpeca = 0;
+ int peca2 = 0, numpeca2 = 0;
+ double  valoruni = 0.0, valoruni2 = 0.0;
+ 
+ scanf("%d\n%d\%lf",&peca,&numpeca,&valoruni);
+ scanf("%d\n%d\%lf",&peca2,&numpeca2,&valoruni2);
+ 
+
+
+
+    return 0;
+}
